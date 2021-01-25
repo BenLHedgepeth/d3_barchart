b23a91f736208fa1c614062000e44814dacef5ce
Ben Hedgepeth <benhh@gmx.com>
2021-01-24 22:31:11 -0800

    Refactored main.js

--- main.js
+++ main.js
@@ -1,60 +1,57 @@
 
 var request = 'https://raw.githubusercontent.com/FreeCodeCamp/'
-request += 'ProjectReferenceData/master/GDP-data.json'
+request += 'ProjectReferenceData/master/GDP-data.json';
 
-const svg_width = 900;
-const svg_height = 500;
+const margin = {
+  top: 30,
+  right: 50,
+  bottom: 30,
+  left: 50
+};
 
-const margin = 50;
-const margin_top = 10;
+const width = 900;
+const innerWidth = width - margin.right - margin.left;
+const height = 500;
+const innerHeight= height - margin.top - margin.bottom;
 
-let svg = d3.select(".contain").append("svg")
-            .attr("width", svg_width).attr("height", svg_height);
+const svg = d3.select(".contain").append('svg')
+      .attr('width', width).attr('height', height);
+
+const g = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+
 
 d3.json(request).then((response) => {
 
-  //Define X axis
-  let years = response.data.reduce(
-    (accumulator, currentValue, i, array) => {
-      let date = new Date(currentValue[0]).getFullYear();
-      if (accumulator.indexOf(date) === -1) {
-        accumulator.push(date);
-      }
-      return accumulator
-    }, []
+  const dates = response.data.map((x) => new Date(x[0]));
+  const gdp_values = response.data.map((x) => x[1]);
+
+  const xScale = d3.scaleTime(
+    d3.extent(dates),
+    [0, innerWidth]
   );
 
-  let dates = d3.extent(response.data, (gdp_data) => new Date(gdp_data[0]))
+  const yScale = d3.scaleLinear(
+    [0, d3.max(gdp_values)],
+    [0, innerHeight],
+  )
 
-  let xScale = d3.scaleTime()
-                  .domain(dates)
-                  .range(0, svg_width - margin);
+  const yAxis = d3.axisLeft(yScale);
+  const xAxis = d3.axisBottom(xScale);
 
-  let xAxis = d3.axisBottom(xScale);
-  svg.append("g")
-      .attr("transform", `translate(${margin}, ${svg_height - margin})`)
-      .call(xAxis)
-
-
-  //Define scale and and axis of Y
-
-  let gdp_values = response.data.map((gdp_data) => +gdp_data[1]);
-  let yScale = d3.scaleLinear()
-                  .domain([0, d3.max(gdp_values)])
-                  .range([svg_height - margin, 0]);
-
-  let g = svg.append("g").attr("transform", `translate(${margin}, ${margin_top})`)
   g.selectAll("rect")
-    .data(gdp_values).enter().append("rect")
-    .attr("width", (svg_width - margin) / gdp_values.length)
-    .attr("height", (d) => (svg_height - margin) - yScale(d))
-    .attr("x", (d, i) => i * ((svg_width - margin) / gdp_values.length))
-    .attr('y', (d) => yScale(d))
-    .attr("class", 'shade');
+    .data(response.data).enter().append('rect')
+    .attr('data-date', (d) => d[0]).attr('data-gdp', (d) => d[1])
+    .attr('x', (d, i) => i * (innerWidth / response.data.length))
+    .attr('y', (d) => innerHeight - yScale(d[1]))
+    .attr('width', (d) => innerWidth / response.data.length)
+    .attr('height', (d) => yScale(d[1]))
+    .attr('class', 'bar')
 
-  let yAxis = d3.axisLeft(yScale);
-  svg.append("g")
-  .attr("transform", `translate(50, 10)`)
-  .call(yAxis)
+  yAxis(g.append('g'));
+  xAxis(g.append('g').attr("transform", `translate(0, ${innerHeight})`));
+
+  console.log(xScale.domain())
 
 })
